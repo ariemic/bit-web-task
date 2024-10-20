79bb4fead179b070bbd14aa5afaffc9340ff0721
Ariel <arie.michalik@gmail.com>
2024-10-20 19:43:51 +0200

    stable

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -1,17 +1,13 @@
-import "reflect-metadata"
-import { DataSource } from "typeorm"
-import { User } from "./entity/User"
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import { User } from "./entity/User";
 
 export const AppDataSource = new DataSource({
-    type: "mysql",
-    host: "localhost",
-    port: 3306,
-    username: "test",
-    password: "test",
-    database: "test",
-    synchronize: true,
-    logging: false,
-    entities: [User],
-    migrations: [],
-    subscribers: [],
-})
+  type: "sqlite",
+  database: "test.db", // Specify the database file name
+  synchronize: true,
+  logging: false,
+  entities: [User],
+  migrations: [],
+  subscribers: [],
+});
